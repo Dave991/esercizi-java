cfc67eaedc3959152aab00e2562e6ae28c66780e
Dave991 <davide.dima991@gmail.com>
2018-10-05 18:08:51 +0200

    esercizio università

---
+++ Università/Docente.java
@@ -0,0 +1,31 @@
+public class Docente {
+
+  private String nome;
+  private String cognome;
+  private String codice;
+  private int eta;
+
+  public class Docente(String nome, String cognome, String codice, int eta) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.codice = codice;
+    this.eta = eta;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public String getCognome() {
+    return this.cognome;
+  }
+
+  public String getCodice() {
+    return this.codice;
+  }
+
+  public int getEta() {
+    return this.eta;
+  }
+  
+}
